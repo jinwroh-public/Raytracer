cb4421b899c4c1b2aadf7583b2931e12fd0d36e4
Jin W. Roh <github.public@jinwroh.com>
2014-10-26 20:58:35 -0700

    Added Point class

---
+++ src/com/jinwroh/raytracer/geometric/Point.java
@@ -0,0 +1,68 @@
+package com.jinwroh.raytracer.geometric;
+
+
+/**
+ * The Point class represents a point in the 3 dimensional Cartesian coordinate 
+ * system. This is an immutable class, since a point should be considered as
+ * a value object that should not be modified.
+ * 
+ * @author	Jin W. Roh
+ * @version	1.0.0
+ */
+final public class Point {
+
+	/**
+	 * The respective x, y, z coordinates of the point
+	 */
+	final private double x;
+	final private double y;
+	final private double z;
+	
+	
+	/**
+	 * Constructs a Point with the respective x, y, z coordinates
+	 * 
+	 * @param x	the x-coordinate
+	 * @param y	the y-coordinate
+	 * @param z	the z-coordinate
+	 */
+	public Point (double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+	/**
+	 * Gets the x-coordinate of this point
+	 * @return	the x-coordinate of this point
+	 */
+	public double getX () {
+		return this.x;
+	}
+	
+	
+	/**
+	 * Gets the y-coordinate of this point
+	 * @return	the y-coordinate of this point
+	 */
+	public double getY () {
+		return this.y;
+	}
+	
+	
+	/**
+	 * Gets the z-coordinate of this point
+	 * @return	the z-coordinate of this point
+	 */
+	public double getZ () {
+		return this.z;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Point: (" + this.x + ", " + this.y + ", " + this.z + ")";
+	}
+}
